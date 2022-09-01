c5ba41195691b10005c74a1aca05c91bce2307cf
Rafael Bini <rfabini1996@gmail.com>
2022-09-01 15:12:01 -0300

    Changed env vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiHost: 'https://bini-cash.herokuapp.com'
+  //apiHost: 'https://bini-cash.herokuapp.com'
+  apiHost: 'https://binicash-backend-production.up.railway.app'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@
 export const environment = {
   production: false,
   apiHost: 'http://localhost:3000'
+  //apiHost: 'https://binicash-backend-production.up.railway.app'
 };
 
